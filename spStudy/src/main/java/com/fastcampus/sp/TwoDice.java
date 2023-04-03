e1a45132e8e75fe13ca9345a1fc7f4a0484802c3
lgswin <lgswin@gmail.com>
2023-04-03 12:11:15 +0900

    TwoDice

---
+++ spStudy/src/main/java/com/fastcampus/sp/TwoDice.java
@@ -0,0 +1,34 @@
+package com.fastcampus.sp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TwoDice {
+
+	@RequestMapping("/rollDice")
+	public void main(HttpServletResponse response) throws IOException {
+		int idx1 = (int)(Math.random()*6)+1;
+		int idx2 = (int)(Math.random()*6)+1;
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("</head>");
+		out.println("<body>");
+		// 주사위를 refesh할때마다 랜덤으로 호출
+		out.println("<img src='resources/img/dice"+idx1+".jpg'>");
+		out.println("<img src='resources/img/dice"+idx2+".jpg'>");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+}
