61c3cb41efbc76ac3ab47e6c770057a9b604a7bb
lgswin <lgswin@gmail.com>
2023-04-24 20:22:34 +0900

    Making a board #1

---
+++ ch4/src/main/java/com/fastcampus/ch4/dao/BoardDao.java
@@ -0,0 +1,23 @@
+package com.fastcampus.ch4.dao;
+
+import com.fastcampus.ch4.domain.*;
+
+import java.util.*;
+
+public interface BoardDao {
+    BoardDto select(Integer bno) throws Exception;
+    int delete(Integer bno, String writer) throws Exception;
+    int insert(BoardDto dto) throws Exception;
+    int update(BoardDto dto) throws Exception;
+    int increaseViewCnt(Integer bno) throws Exception;
+
+    List<BoardDto> selectPage(Map map) throws Exception;
+    List<BoardDto> selectAll() throws Exception;
+    int deleteAll() throws Exception;
+    int count() throws Exception;
+/*
+    int searchResultCnt(SearchCondition sc) throws Exception;
+    List<BoardDto> searchSelectPage(SearchCondition sc) throws Exception;
+
+ */
+}
